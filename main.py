2b8b70d1c756ec92621682025856945c984f391b
Vladimir <49948201+4anky@users.noreply.github.com>
2019-05-22 01:07:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import random
 from PyQt5.QtWidgets import QApplication, QMainWindow, QGraphicsScene, QGraphicsRectItem
 from PyQt5.QtGui import QColor
 from PyQt5.QtCore import Qt
-
 import numpy as np
 
 
@@ -19,16 +18,27 @@ class Main(QMainWindow):
         self.ui = gui.Ui_Tetris()
         self.ui.setupUi(self)
 
-        self.x0, self.y0 = 5, 0
+        self.point = [5, 0]
         self.size = 40
         self.scene = QGraphicsScene(0, 0, 400, 800)
         self.ui.view_cup.setScene(self.scene)
 
+        self.score = 0
+        self.ui.text_score.setText(str(self.score))
+
+        self.vectors = {'I': [[0, 1], [0, 2], [0, 3]],
+                        'T': [[-1, 1], [0, 1], [1, 1]],
+                        'O': [[-1, 0], [-1, 1], [0, 1]],
+                        'Z': [[-1, 0], [0, 1], [1, 1]],
+                        'S': [[1, 0], [0, 1], [-1, 1]],
+                        'L': [[0, 1], [0, 2], [1, 2]],
+                        'J': [[0, 1], [0, 2], [-1, 2]]}
+
         self.cup = np.zeros((20, 10), dtype=int)
         self.w = self.cup.shape[1]
         self.h = self.cup.shape[0]
-        self.cup[self.y0][self.x0] = random.randint(1, 7)
-
+        for i in self.fig_choice[str(random.randint(1, 7))]:
+            self.cup[i[1]][i[0]]
 
         self.ui.btn_newgame.clicked.connect(self.game_on)
 
@@ -36,48 +46,120 @@ class Main(QMainWindow):
                       QColor(255, 255, 0), QColor(255, 0, 255), QColor(255, 128, 0), QColor(128, 64 ,0),
                       QColor(0, 0, 0)]
 
-    def keyPressEvent(self, event, x0, y0, cup):
-        if event.key() == Qt.Key_A and x0:
-            x0, y0 = x0 - 1, y0
-            cup[y0][x0] = cup[y0][x0 + 1]
-            cup[y0][x0 + 1] = 0
-            return x0, y0, cup
+        self.fig_choice = {'1': self.i_fig(),
+                           '2': self.t_fig(),
+                           '3': self.o_fig(),
+                           '4': self.z_fig(),
+                           '5': self.s_fig(),
+                           '6': self.l_fig(),
+                           '7': self.j_fig()}
 
+    def i_fig(self):
+        self.i_vect = self.vectors['I']
+        return [self.point,
+                [self.point[0] + self.i_vect[0][0], self.point[1] + self.i_vect[0][1]],
+                [self.point[0] + self.i_vect[1][0], self.point[1] + self.i_vect[1][1]],
+                [self.point[0] + self.i_vect[2][0], self.point[1] + self.i_vect[2][1]]]
+
+    def t_fig(self):
+        self.t_vect = self.vectors['T']
+        return [self.point,
+                [self.point[0] + self.t_vect[0][0], self.point[1] + self.t_vect[0][1]],
+                [self.point[0] + self.t_vect[1][0], self.point[1] + self.t_vect[1][1]],
+                [self.point[0] + self.t_vect[2][0], self.point[1] + self.t_vect[2][1]]]
+
+    def o_fig(self):
+        self.o_vect = self.vectors['O']
+        return [self.point,
+                [self.point[0] + self.o_vect[0][0], self.point[1] + self.o_vect[0][1]],
+                [self.point[0] + self.o_vect[1][0], self.point[1] + self.o_vect[1][1]],
+                [self.point[0] + self.o_vect[2][0], self.point[1] + self.o_vect[2][1]]]
+
+    def z_fig(self):
+        self.z_vect = self.vectors['Z']
+        return [self.point,
+                [self.point[0] + self.z_vect[0][0], self.point[1] + self.z_vect[0][1]],
+                [self.point[0] + self.z_vect[1][0], self.point[1] + self.z_vect[1][1]],
+                [self.point[0] + self.z_vect[2][0], self.point[1] + self.z_vect[2][1]]]
+
+    def s_fig(self):
+        self.s_vect = self.vectors['S']
+        return [self.point,
+                [self.point[0] + self.s_vect[0][0], self.point[1] + self.s_vect[0][1]],
+                [self.point[0] + self.s_vect[1][0], self.point[1] + self.s_vect[1][1]],
+                [self.point[0] + self.s_vect[2][0], self.point[1] + self.s_vect[2][1]]]
+
+    def l_fig(self):
+        self.l_vect = self.vectors['L']
+        return [self.point,
+                [self.point[0] + self.l_vect[0][0], self.point[1] + self.l_vect[0][1]],
+                [self.point[0] + self.l_vect[1][0], self.point[1] + self.l_vect[1][1]],
+                [self.point[0] + self.l_vect[2][0], self.point[1] + self.l_vect[2][1]]]
+
+    def j_fig(self):
+        self.j_vect = self.vectors['J']
+        return [self.point,
+                [self.point[0] + self.s_vect[0][0], self.point[1] + self.s_vect[0][1]],
+                [self.point[0] + self.s_vect[1][0], self.point[1] + self.s_vect[1][1]],
+                [self.point[0] + self.s_vect[2][0], self.point[1] + self.s_vect[2][1]]]
+
+
+
+    def update_scene(self):
+        self.scene.clear()
+        [self.scene.addRect(self.size * i, self.size * j, self.size, self.size,
+                            self.color[0], self.color[self.cup[j][i]]) for i in range(self.w) for j in range(self.h)]
+
+    def shift_left(self):
+        self.point = [self.point[0] - 1, self.point[1]]
+        self.cup[self.point[1]][self.point[0]] = self.cup[self.point[1]][self.point[0] + 1]
+        self.cup[self.point[1]][self.point[0] + 1] = 0
+        self.update_scene()
+
+    def shift_right(self):
+        self.point = [self.point[0] + 1, self.point[1]]
+        self.cup[self.point[1]][self.point[0]] = self.cup[self.point[1]][self.point[0] - 1]
+        self.cup[self.point[1]][self.point[0] - 1] = 0
+        self.update_scene()
+
+    def shift_down(self):
+        self.point = [self.point[0], self.point[1] + 1]
+        self.cup[self.point[1]][self.point[0]] = self.cup[self.point[1] - 1][self.point[0]]
+        self.cup[self.point[1] - 1][self.point[0]] = 0
+        self.update_scene()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_A and self.point[0] and not self.cup[self.point[1]][self.point[0] - 1]:
+            self.shift_left()
+
+        if event.key() == Qt.Key_D and self.point[0] < 9 and not self.cup[self.point[1]][self.point[0] + 1]:
+            self.shift_right()
+
+        if event.key() == Qt.Key_S and self.point[1] < 19 and not self.cup[self.point[1] + 1][self.point[0]]:
+            self.shift_down()
 
     def game_on(self):
         self.timer_id = self.startTimer(200, timerType=Qt.PreciseTimer)
 
     def timerEvent(self, event):
-        if self.y0 < 19 and not self.cup[self.y0 + 1][self.x0]:
-
-            self.x0, self.y0 = self.x0, self.y0 + 1
-            self.cup[self.y0][self.x0] = self.cup[self.y0 - 1][self.x0]
-            self.cup[self.y0 - 1][self.x0] = 0
-
-            self.scene.clear()
-            [self.scene.addRect(self.size * i, self.size * j, self.size, self.size,
-                            self.color[0], self.color[self.cup[j][i]]) for i in range(self.w) for j in range(self.h)]
+        if 0 in [self.point[1] < 19 and not self.cup[self.point[1] + 1][self.point[0]]]:
+            self.shift_down()
         else:
-            self.x0, self.y0 = 5, 0
-            self.cup[self.y0][self.x0] = random.randint(1, 7)
+            for i in range(len(self.cup)):
+                if not (0 in self.cup[i]):
+                    self.cup[i] = np.zeros((1, 10), dtype=int)
+                    self.cup = np.roll(self.cup, 1, axis=0)
+                    self.score += 100
+                    self.ui.text_score.setText(str(self.score))
+
+            self.point = [5, 0]
+            self.cup[self.point[1]][self.point[0]] = random.randint(1, 7)
+            self.update_scene()
 
 class Figures():
     def __init__(self):
         pass
-    def i_fig(self):
-        pass
-    def t_fig(self):
-        pass
-    def z_fig(self):
-        pass
-    def s_fig(self):
-        pass
-    def o_fig(self):
-        pass
-    def l_fig(self):
-        pass
-    def j_fig(self):
-        pass
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- test.py
+++ test.py
@@ -1,4 +1,69 @@
-import numpy as np
+vectors = {'I': [[0, 1], [0, 2], [0, 3]],
+            'T': [[-1, 1], [0, 1], [1, 1]],
+            'O': [[-1, 0], [-1, 1], [0, 1]],
+            'Z': [[-1, 0], [0, 1], [1, 1]],
+            'S': [[1, 0], [0, 1], [-1, 1]],
+            'L': [[0, 1], [0, 2], [1, 2]],
+            'J': [[0, 1], [0, 2], [-1, 2]]}
 
-a = np.zeros((20, 10), dtype=int)
-print(a[20][0])
+point = [0, 5]
+
+def i_fig():
+    i_vect = vectors['I']
+    return [point,
+                [point[0] + i_vect[0][0], point[1] + i_vect[0][1]],
+                [point[0] + i_vect[1][0], point[1] + i_vect[1][1]],
+                [point[0] + i_vect[2][0], point[1] + i_vect[2][1]]]
+
+def t_fig():
+    t_vect = vectors['T']
+    return [point,
+                [point[0] + t_vect[0][0], point[1] + t_vect[0][1]],
+                [point[0] + t_vect[1][0], point[1] + t_vect[1][1]],
+                 [point[0] + t_vect[2][0], point[1] + t_vect[2][1]]]
+
+def o_fig():
+    o_vect = vectors['O']
+    return [point,
+               [point[0] + o_vect[0][0], point[1] + o_vect[0][1]],
+               [point[0] + o_vect[1][0], point[1] + o_vect[1][1]],
+               [point[0] + o_vect[2][0], point[1] + o_vect[2][1]]]
+
+def z_fig():
+    z_vect = vectors['Z']
+    return [point,
+               [point[0] + z_vect[0][0], point[1] + z_vect[0][1]],
+               [point[0] + z_vect[1][0], point[1] + z_vect[1][1]],
+               [point[0] + z_vect[2][0], point[1] + z_vect[2][1]]]
+
+def s_fig():
+    s_vect = vectors['S']
+    return [point,
+                [point[0] + s_vect[0][0], point[1] + s_vect[0][1]],
+                [point[0] + s_vect[1][0], point[1] + s_vect[1][1]],
+                [point[0] + s_vect[2][0], point[1] + s_vect[2][1]]]
+
+def l_fig():
+    l_vect = vectors['L']
+    return [point,
+               [point[0] + l_vect[0][0], point[1] + l_vect[0][1]],
+               [point[0] + l_vect[1][0], point[1] + l_vect[1][1]],
+               [point[0] + l_vect[2][0], point[1] + l_vect[2][1]]]
+
+def j_fig():
+    j_vect = vectors['J']
+    return [point,
+                 [point[0] + j_vect[0][0], point[1] + j_vect[0][1]],
+                 [point[0] + j_vect[1][0], point[1] + j_vect[1][1]],
+                 [point[0] + j_vect[2][0], point[1] + j_vect[2][1]]]
+
+fig_choice = {'1': i_fig(),
+              '2': t_fig(),
+              '3': o_fig(),
+              '4': z_fig(),
+              '5': s_fig(),
+              '6': l_fig(),
+              '7': j_fig()}
+
+print(fig_choice['2'])
+
