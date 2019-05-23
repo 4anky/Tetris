da994461664894bd36aba64ed3093694d9365665
Vladimir <49948201+4anky@users.noreply.github.com>
2019-05-23 14:43:40 +0300

    Немного рабочая версия
    Особенности:
    1. Если заполняется > 1 строки, то пропадают не все заполненные, а только одна из них. Также не учитывается показательный рост очков при заполнении > 1 строки;
    2. Отсутствует функция поворота фигур;
    3. В квадрате (5;0) какая-то аномалия.

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ class Figures:
     def __init__(self):
         self.point = [5, 0]
         self.color = [QColor(255, 255, 255), QColor(255, 0, 0), QColor(0, 255, 0), QColor(0, 0, 255),
-                      QColor(255, 255, 0), QColor(255, 0, 255), QColor(255, 128, 0), QColor(128, 64, 0),
-                      QColor(0, 0, 0)]
+                      QColor(255, 255, 0), QColor(255, 0, 255), QColor(255, 128, 0), QColor(128, 64, 0)]
         self.vectors_collection = {1: [[0, 1], [0, 2], [0, 3]],
                                    2: [[-1, 1], [0, 1], [1, 1]],
                                    3: [[-1, 0], [-1, 1], [0, 1]],
@@ -41,100 +40,165 @@ class Main(QMainWindow):
         self.ui.setupUi(self)
         self.figures = Figures()
 
-        self.size = 40
         self.score = 0
+        self.matrix = 0
+        self.new_tetramino_flag = True
+
         self.scene = QGraphicsScene(0, 0, 400, 800)
         self.ui.view_cup.setScene(self.scene)
-
-        self.cup = np.zeros((20, 10), dtype=int)
-
-        self.num = random.randint(1, 7)
-        self.tetramino = self.figures.fig(self.figures.vectors_collection[self.num], self.figures.point)
-        for i in self.tetramino[1:5]:
-            self.cup[i[1]][i[0]] = self.num
 
         self.ui.text_score.setText(str(self.score))
         self.ui.btn_newgame.clicked.connect(self.game_on)
 
+    def cup_set(self, point, num):
+        self.matrix[point[1]][point[0]] = num
+
+    def cup_get(self, point):
+        return self.matrix[point[1]][point[0]]
+
+
     def check_down(self):
-        self.set_of_down_y = {}
-        for i in self.tetramino[1:5]:
-            if not (self.set_of_down_y.get(i[0])) or self.set_of_down_y[i[0]] < i[1]:
-                self.set_of_down_y[i[0]] = i[1]
-        if 19 in self.set_of_down_y.values():
-            return False
-        self.res = []
-        [self.res.append(self.cup[self.set_of_down_y[i] + 1][i]) for i in self.set_of_down_y.keys()]
-        if not sum(self.res):
+        self.down_y = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+        self.under_down_y = []
+
+        for i in range(1, 5):
+            if self.tetramino[i][1] > self.down_y[self.tetramino[i][0]]:
+                self.down_y[self.tetramino[i][0]] = self.tetramino[i][1]
+
+        for i in range(10):
+            if self.down_y[i] >= 0:
+                if self.down_y[i] >= 19:
+                    return False
+                else:
+                    self.under_down_y.append(self.matrix[self.down_y[i] + 1][i])
+            else:
+                continue
+
+        if not sum(self.under_down_y):
             return True
         else:
             return False
 
     def check_left(self):
-        pass
+        self.left_x = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+        self.left_left_x = []
+
+        for i in range(1, 5):
+            if self.tetramino[i][0] < self.left_x[self.tetramino[i][1]]:
+                self.left_x[self.tetramino[i][1]] = self.tetramino[i][0]
+
+        for i in range(20):
+            if self.left_x[i] <= 9:
+                if self.left_x[i] <= 0:
+                    return False
+                else:
+                    self.left_left_x.append(self.matrix[i][self.left_x[i] - 1])
+            else:
+                continue
+
+        if not sum(self.left_left_x):
+            return True
+        else:
+            return False
 
     def check_right(self):
-        pass
+        self.right_x = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+        self.right_right_x = []
+
+        for i in range(1, 5):
+            if self.tetramino[i][0] > self.right_x[self.tetramino[i][1]]:
+                self.right_x[self.tetramino[i][1]] = self.tetramino[i][0]
+
+        for i in range(20):
+            if self.right_x[i] >= 0:
+                if self.right_x[i] >= 9:
+                    return False
+                else:
+                    self.right_right_x.append(self.matrix[i][self.right_x[i] + 1])
+            else:
+                continue
+
+        if not sum(self.right_right_x):
+            return True
+        else:
+            return False
+
 
     def update_scene(self):
         self.scene.clear()
-        [self.scene.addRect(self.size * i, self.size * j, self.size, self.size,
-                            self.figures.color[0], self.figures.color[self.cup[j][i]]) for i in range(self.cup.shape[1]) for j in range(self.cup.shape[0])]
+        for i in range(10):
+            for j in range(20):
+                self.scene.addRect(40 * i, 40 * j, 40, 40, self.figures.color[0], self.figures.color[self.matrix[j][i]])
 
     def score_and_delete_line(self):
-        for i in range(len(self.cup)):
-            if not (0 in self.cup[i]):
-                self.cup[i] = np.zeros((1, 10), dtype=int)
-                self.cup = np.roll(self.cup, 1, axis=0)
+        for i in range(20):
+            if not (0 in self.matrix[i]):
+                self.matrix[i] = np.zeros((1, 10), dtype=int)
+                self.matrix[0:i+1] = np.roll(self.matrix[0:i+1], 1, axis=0)
                 self.score += 100
-                self.ui.text_score.setText(str(self.score))
+        self.ui.text_score.setText(str(self.score))
 
-    def shift_left(self):
-        self.update_scene()
-
-    def shift_right(self):
-        self.update_scene()
 
     def shift_down(self):
-            for i in self.tetramino[2:5]:
-                self.cup[i[1]][i[0]] = 0
+        for i in range(1, 5):
+            self.cup_set(self.tetramino[i], 0)
+            self.tetramino[i][1] += 1
 
-            self.tetramino[1][1] += 1
-            self.cup[self.tetramino[1][1]][self.tetramino[1][0]] = self.cup[self.tetramino[1][1] - 1][self.tetramino[1][0]]
-            self.cup[self.tetramino[1][1] - 1][self.tetramino[1][0]] = 0
+        for i in range(1, 5):
+            self.cup_set(self.tetramino[i], self.num)
 
-            self.tetramino = self.figures.fig(self.figures.vectors_collection[self.num], self.tetramino[1])
-            self.cup[self.tetramino[2][1]][self.tetramino[2][0]] = self.num
-            self.cup[self.tetramino[3][1]][self.tetramino[3][0]] = self.num
-            self.cup[self.tetramino[4][1]][self.tetramino[4][0]] = self.num
+    def shift_left(self):
+        for i in range(1, 5):
+            self.cup_set(self.tetramino[i], 0)
+            self.tetramino[i][0] -= 1
 
-            self.update_scene()
+        for i in range(1, 5):
+            self.cup_set(self.tetramino[i], self.num)
+
+    def shift_right(self):
+        for i in range(1, 5):
+            self.cup_set(self.tetramino[i], 0)
+            self.tetramino[i][0] += 1
+
+        for i in range(1, 5):
+            self.cup_set(self.tetramino[i], self.num)
+
 
     def keyPressEvent(self, event):
-        if event.key() == Qt.Key_A and self.point[0] and not self.cup[self.point[1]][self.point[0] - 1]:
-            self.shift_left()
-
-        if event.key() == Qt.Key_D and self.point[0] < 9 and not self.cup[self.point[1]][self.point[0] + 1]:
-            self.shift_right()
-
-        if event.key() == Qt.Key_S and self.point[1] < 19 and not self.cup[self.point[1] + 1][self.point[0]]:
+        if event.key() == Qt.Key_S and self.check_down():
             self.shift_down()
-
-    def game_on(self):
-        self.timer_id = self.startTimer(200, timerType=Qt.PreciseTimer)
-
-    def timerEvent(self, event):
-        if self.check_down():
-            self.shift_down()
-        else:
-            self.score_and_delete_line()
             self.update_scene()
 
-            self.num = random.randint(1, 7)
-            self.tetramino = self.figures.fig(self.figures.vectors_collection[self.num], self.figures.point)
+        if event.key() == Qt.Key_A and self.check_left():
+            self.shift_left()
+            self.update_scene()
 
-            for i in self.tetramino[1:5]:
-                self.cup[i[1]][i[0]] = self.num
+        if event.key() == Qt.Key_D and self.check_right():
+            self.shift_right()
+            self.update_scene()
+
+    def game_on(self):
+        self.matrix = np.zeros((20, 10), dtype=int)
+        self.timer_id = self.startTimer(100, timerType=Qt.PreciseTimer)
+
+    def timerEvent(self, event):
+        if self.new_tetramino_flag:
+            self.num = random.randint(1, 7)
+            self.tetramino = self.figures.fig(self.figures.vectors_collection[self.num], [5, 0])
+
+            if not sum([self.cup_get(self.tetramino[i]) for i in range(1, 5)]):
+                [self.cup_set(i, self.num) for i in self.tetramino[1:5]]
+                self.update_scene()
+                self.new_tetramino_flag = False
+            else:
+                print('GAME OVER!!!')
+
+        if self.check_down():
+            self.shift_down()
+            self.update_scene()
+        else:
+            self.new_tetramino_flag = True
+            self.score_and_delete_line()
             self.update_scene()
 
 if __name__ == '__main__':
